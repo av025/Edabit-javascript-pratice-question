d3d10f188a9da129e6bf2cc5ae922f3fa734c0d5
Aayush Vyas <aayush25vyas@gmail.com>
2024-07-08 23:31:38 +0530

    Squared of the given number

---
+++ 1.Very Easy/squared of the given number/index.js
@@ -0,0 +1,7 @@
+const squared = (givenNumber) => {
+    return givenNumber * givenNumber;
+}
+
+console.log(squared(5)); //* Output was 25
+console.log(squared(9)); //* Output was 81 
+console.log(squared(100)); //* Output was 10000
